a9847448cffb15b8ce157f31a9a7777e94adeaef
Sathvik C <sathvikchekkali123@gmail.com>
2024-01-18 12:14:00 +0530

    maven java push

---
+++ src/main/java/Se_lab/KMIT_Java3/App.java
@@ -0,0 +1,13 @@
+package Se_lab.KMIT_Java3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
